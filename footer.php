8356197d52282ebe5353337493db41d65eb246fb
Igor Benić <igorbenic@users.noreply.github.com>
2014-11-06 15:52:48 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+
+<footer id="footer">
+	<div class="container">
+		<p class="text-center">&copy; <?php echo date("Y") ?>, <?php echo bloginfo('name' ); ?></p>
+	</div>
+</footer>
+
+    	<?php wp_footer(); ?>
+	</body>
+</html>
